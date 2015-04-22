06e85af98556ff3f901df2160a858034fdaf02da
Kostya Golikov <johnysilver7@gmail.com>
2015-04-22 21:15:12 +0300

    Added implementation of fizz buzz program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// "Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz”
+// instead of the number and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”."
+
 fn main() {
-    println!("Hello, world!");
+    for x in 0..100 {
+        match x {
+            _ if x % 3 == 0 && x % 5 == 0 => println!("FizzBuzz"),
+            _ if x % 3 == 0 => println!("Fizz"),
+            _ if x % 5 == 0 => println!("Buzz"),
+            number => println!("{}", number)
+        }
+
+    }
 }
